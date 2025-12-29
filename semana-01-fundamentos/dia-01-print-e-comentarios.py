05ec0ef85f12486702c96ee3d76ead9b96818d45
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-29 18:08:33 +0900

    Dia 1: saída de dados e comentários em Python

---
+++ semana-01-fundamentos/dia-01-print-e-comentarios.py
@@ -0,0 +1,22 @@
+"""
+Arquivo: dia-01-print-e-comentarios.py
+Objetivo: Primeiro contato com Python e saída de dados usando print()
+"""
+
+# Este é um comentário de uma linha
+# Comentários servem para explicar o código (não são executados)
+
+print("Olá, mundo!")
+
+# Podemos imprimir textos diferentes
+print("Iniciando meus estudos em Python")
+
+# Também é possível imprimir números
+print(10)
+print(3.14)
+
+# Python executa o código de cima para baixo
+print("Fim do programa")
+
+print("Meu nome é Brian")
+print("Estou estudando Python para treinar lógica de programação")
