4adb0bbfbc10170cc7296a4374d80ecb87d16b6c
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-30 19:36:15 +0900

    Create dia-2-variaveis-e-tipos.py

---
+++ semana-01-fundamentos/dia-2-variaveis-e-tipos.py
@@ -0,0 +1,36 @@
+"""
+Arquivo: dia-02-variaveis-e-tipos.py
+Obejtivo: Trabalhar com variáveis e tipos básicos em Phyton.
+"""
+#Variáveis do tipo inteiro
+idade = 30
+ano = 2025
+
+print(idade)
+print(ano)
+
+#Variáveis do tipo decimal(float)
+altura =1.75
+peso = 70.5
+
+print(altura)
+print(peso)
+
+#VAriáveis do tipo texto(string)
+
+nome = "Brian"
+cidade = "Tokyo"
+
+print(nome)
+print(cidade)
+
+#Exercício: adicionar variável com minha idade, um valor decima e meu objetivo ao estudar Phyton
+
+minha_idade = 40
+minha_altura = 1.55
+objetivo = "Estudar Phyton para conseguir um emprego na área de TI no Brasil."
+
+print(minha_idade)
+print(minha_altura)
+print(objetivo)
+
