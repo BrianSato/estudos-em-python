92f986887fcc77833407432cc31fedf1fb85c40f
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-11 09:14:30 +0900

    refactor: reorganizar estrutura do projeto

---
+++ semana-04-gerenciador-despesas/despesas_listar.py
@@ -0,0 +1,12 @@
+def listar_despesas(despesas):
+    if not despesas:
+        print("Nenhuma despesa adicionada")
+        return
+    print("------LISTA DE DESPESAS------")
+
+    for i, despesa in enumerate(despesas, start=1):
+        print(
+            f"{i}. R$ {despesa['valor']:.2f} - "
+            f"{despesa['descricao']} - "
+            f"{despesa.get('categoria', 'Sem categoria')}"
+        )
