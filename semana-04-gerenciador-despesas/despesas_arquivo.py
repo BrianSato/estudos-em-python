4e4bcda44bc4be30a70cb6039589ee573171882b
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-11 09:18:55 +0900

    refactor: Alteração de arquivo.txt para JSON

--- semana-04-gerenciador-despesas/despesas_arquivo.py
+++ semana-04-gerenciador-despesas/despesas_arquivo.py
@@ -1,21 +1,20 @@
+import json
+import os
+
+ARQUIVO = "despesas.json"
+
 #  FUNCOES PARA SALVAR  E CARREGAR OS DADOS EM ARQUIVO
 
 def salva_despesas(despesas):
-    with open("despesas.txt", "w", encoding="utf-8") as arquivo:
-        for despesa in despesas:
-            linha = f"{despesa['valor']} | {despesa['descricao']}\n"
-            arquivo.write(linha)
-            
+    with open(ARQUIVO, "w", encoding="utf-8") as f:
+        json.dump(despesas,f,ensure_ascii=False, indent=4)
+
 def carregar_despesas():
-    lista_despesas = []
-    try:
-        with open("despesas.txt", "r", encoding="utf-8") as arquivo:
-            for linha in arquivo:
-                valor, descricao = linha.strip().split(" | ")
-                lista_despesas.append({
-                    "valor": float(valor),
-                    "descricao": descricao
-                })
-    except FileNotFoundError:
-        pass    #arquivo ainda não existe
-    return lista_despesas
+    if not os.path.exists(ARQUIVO):
+        return[]
+
+    with open(ARQUIVO, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return[]
