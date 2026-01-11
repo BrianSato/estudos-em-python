f9f67b2509f7924a52d38a1f71e532eb9797bf44
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-11 09:21:48 +0900

    refactor: reorganizar estrutura do projeto

---
+++ semana-04-gerenciador-despesas/despesas_menu.py
@@ -0,0 +1,7 @@
+def menu():
+    print("\n-------GERENCIADOR DE DESPESAS-------")
+    print(" 1 - Adicionar despesa:")
+    print(" 2 - Listar despesas:")
+    print(" 3 - Filtrar despesas por Categoria")
+    print(" 4 - Ver estatisticas:")
+    print(" 0 - Sair")
