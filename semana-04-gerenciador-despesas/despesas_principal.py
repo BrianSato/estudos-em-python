69b0979ad9b244fbf850e70efb655c74bb1cea89
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-11 09:25:12 +0900

    refactor: modularizar código por responsabilidade

--- semana-04-gerenciador-despesas/despesas_principal.py
+++ semana-04-gerenciador-despesas/despesas_principal.py
@@ -1,13 +1,18 @@
 #IMPORTANDO FUNCOES
-import despesas_funcoes_vs01 as df
-import despesas_arquiva_vs01 as da
+import despesas_menu as desp_menu
+import despesas_arquivo as desp_arq
+import despesas_listar as desp_list
+import despesas_calculos as desp_calc
+from despesas_filtrar import filtrar_por_categoria as catg_filt
+from despesas_adicionar import escolher_categorias as catg_esc
+from despesas_adicionar import adiciona_despesa as desp_add
 
 #CARREGANDO DO ARQUIVO
-despesas = da.carregar_despesas()
+despesas = desp_arq.carregar_despesas()
 
 # INICIO DO PROGRAMA
 while True:
-    df.menu()
+    desp_menu.menu()
     try:
         opcao = int(input(" DIGITE UMA DAS OPÇÕES ACIMA:"))
     except ValueError:
@@ -15,17 +20,30 @@ while True:
         continue
 
     if opcao == 1:
-        df.adiciona_despesa(despesas)
+        desp_add(despesas)
     elif opcao == 2:
-        df.listar_despesas(despesas)
+        desp_list.listar_despesas(despesas)
     elif opcao == 3:
-        df.mostrar_total_e_media(despesas)
-        df.maior_menor_valor(despesas)
+        categoria = catg_esc()
+        filtradas = catg_filt(despesas,categoria)
+        if not filtradas:
+            print("Nenhuma categoria encontrada para esta categoria")
+        else:
+            print(f"---- Categoria : {categoria} ----")
+            for i, despesa in enumerate(filtradas, start=1):
+                print(
+                    f"{i}. R$ {despesa['valor']:.2f} - "
+                    f"{despesa['descricao']}"
+                )
+    elif opcao == 4:
+        desp_calc.mostrar_total_e_media(despesas)
+        desp_calc.maior_menor_valor(despesas)
     elif  opcao == 0:
         print("👋 Programa encerrado.")
         break
+
     else:
         print("❌ Opção inválida.")
 
-da.salva_despesas(despesas)
+desp_arq.salva_despesas(despesas)
 print("💾 Despesas salvas com sucesso.")
