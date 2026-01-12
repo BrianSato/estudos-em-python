de9b2def8d559c612e2a4a1ea3c0ac1a2b3ef356
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-13 05:22:19 +0900

    feat: adicionar data às despesas
    Inclusão de campo de data na listagem de despesas.

--- semana-04-gerenciador-despesas/despesas_listar.py
+++ semana-04-gerenciador-despesas/despesas_listar.py
@@ -6,7 +6,8 @@ def listar_despesas(despesas):
 
     for i, despesa in enumerate(despesas, start=1):
         print(
-            f"{i}. R$ {despesa['valor']:.2f} - "
+            f"{i}.{despesa.get('data', 'sem data')} - " 
+            f" R$ {despesa['valor']:.2f} - "
             f"{despesa['descricao']} - "
             f"{despesa.get('categoria', 'Sem categoria')}"
         )
